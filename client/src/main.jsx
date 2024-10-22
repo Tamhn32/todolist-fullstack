6556373d001f1f4b89bfa593a6bce8b078074e9a
Tam Nguyen <hoangtam32@gmail.com>
2024-10-21 21:48:14 -0500

    adding login stuff

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./index.css";
 import router from "./routers";
+import { Container } from "@mui/material";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Container maxWidth="lg" sx={{ textAlign: "center", marginTop: "50px" }}>
+      <RouterProvider router={router} />
+    </Container>
   </StrictMode>
 );
